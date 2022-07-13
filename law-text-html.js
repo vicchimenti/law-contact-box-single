63e1c0a3dad96c88eab92a429284fc2174c5184e
Vic Chimenti <victor.chimenti@gmail.com>
2022-07-13 08:26:44 -0700

    email logic

--- law-text-html.js
+++ law-text-html.js
@@ -252,6 +252,15 @@ try {
                                     ? '<p class="contactBoxSingleInfoEmail"><span class="fas fa-envelope"></span>&nbsp;<a href="mailto:' + contentDict.email.content + '" aria-label="Send an email to ' + emailTitle + '">Contact ' + emailTitle + '</a></p>'
                                     : '<p class="contactBoxSingleInfoEmail visually-hidden">No Email Provided</p>';
 
+    let emailString = (contentDict.email.content && contentDict.firstName.content) ?
+        '<p class="contactBoxSingleInfoEmail"><span class="fas fa-envelope"></span>&nbsp;<a href="mailto:' + contentDict.email.content + '" aria-label="Send an email to ' + contentDict.firstName.content + '">Contact ' + contentDict.firstName.content + '</a></p>' :
+        (contentDict.email.content && contentDict.fullName.content) ?
+        '<p class="contactBoxSingleInfoEmail"><span class="fas fa-envelope"></span>&nbsp;<a href="mailto:' + contentDict.email.content + '" aria-label="Send an email to ' + contentDict.fullName.content + '">Contact ' + contentDict.fullName.content + '</a></p>' :
+        '<p class="contactBoxSingleInfoEmail visually-hidden">No Email Provided</p>';
+
+
+
+
 
 
 

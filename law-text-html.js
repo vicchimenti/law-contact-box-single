452e69c9ab5f198e9a3925e5b8a21b70b2bd937d
vicchimenti <chimenti@seattleu.edu>
2022-07-14 07:01:43 -0700

    single level ternary

--- law-text-html.js
+++ law-text-html.js
@@ -171,9 +171,7 @@ try {
      *  Parse for Bio Link
      * 
      * */
-    let profileString = (contentDict.profileLinkUrl.content && contentDict.firstName.content) ?
-        '<p class="contactBoxSingleInfoProfile"><span class="fas fa-user"></span>&nbsp;<a href="' + contentDict.profileLinkUrl.content + '" aria-label="Visit ' + contentDict.profileLinkText.content + '">Visit the Full Profile</a></p>' :
-        (contentDict.profileLinkUrl.content) ?
+    let profileString = (contentDict.profileLinkUrl.content) ?
         '<p class="contactBoxSingleInfoProfile"><span class="fas fa-user"></span>&nbsp;<a href="' + contentDict.profileLinkUrl.content + '" aria-label="Visit ' + contentDict.profileLinkText.content + '">' + contentDict.profileLinkText.content + '</a></p>' :
         '<p class="contactBoxSingleInfoProfile visually-hidden">No Email Provided</p>';
 
